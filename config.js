d995d7c7ba43407b469dfaf9aae642cd21a05194
IuriFrancezz <69679783+IuriFrancezz@users.noreply.github.com>
2020-11-24 23:19:39 +0000

    Fixed Bugs (Uploud test files)
    I uploud test files (testing functions, now aren´t here more) :)

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
-function ExecutarVariavel(nota1, nota2, nota3, nota4, media, total) {
+function confirmarNota (nota1, nota2, nota3, nota4, media, total) {
+    var nota1 = document.getElementById("nota1").value;
+    var nota2 = document.getElementById("nota2").value;
+    var nota3 = document.getElementById("nota3").value;
+    var nota4 = document.getElementById("nota4").value;
+
+    var media = document.getElementById("media").value;
 
     media = 4;
-    total = (nota1 + nota2 + nota3 + nota4) / media;
+    total = (nota1 * nota2 * nota3 * nota4) / media;
 
-    document.getElementById("media").innerHTML = "A Média da tua Nota é TESTE";  
-}
+    document.getElementById("media").innerHTML = "A Média da tua Nota é: " + total + "%";
+  }
