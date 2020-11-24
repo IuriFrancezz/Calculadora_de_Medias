ee6a97748d3c00350b1a6aad518661403a301cbd
IuriFrancezz <69679783+IuriFrancezz@users.noreply.github.com>
2020-11-24 23:16:02 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+function ExecutarVariavel(nota1, nota2, nota3, nota4, media, total) {
+
+    media = 4;
+    total = (nota1 + nota2 + nota3 + nota4) / media;
+
+    document.getElementById("media").innerHTML = "A Média da tua Nota é TESTE";  
+}
