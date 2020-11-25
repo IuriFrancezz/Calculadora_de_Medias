344ca2f0bd9fab4109b8b44fee1ac445b3acccc8
IuriFrancezz <69679783+IuriFrancezz@users.noreply.github.com>
2020-11-25 17:06:03 +0000

    Fixed Some Problems (HTML, JavaScript)

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
-function confirmarNota (nota1, nota2, nota3, nota4, media, total) {
+function confirmarNota (nota1, nota2, nota3, nota4, media) {
     var nota1 = document.getElementById("nota1").value;
     var nota2 = document.getElementById("nota2").value;
     var nota3 = document.getElementById("nota3").value;
     var nota4 = document.getElementById("nota4").value;
 
-    var media = document.getElementById("media").value;
 
-    media = 4;
-    total = (nota1 + nota2 + nota3 + nota4) / media;
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-    document.getElementById("media").innerHTML = "A Média da tua Nota é: " + total + "%";
+    if( media < 10) {
+      mensagem = "A Média da tua Nota é " + media + " \n\n-> Reprovas";
+      }
+      else if (media >= 10 && media < 14) {
+      mensagem = "A Média da tua Nota é " + media + " \n\n-> Vai a exame";
+      }
+      else {
+      mensagem = "A Média da tua Nota é " + media + " \n\n-> Dispensa de exame";
+      }
+      
+      alert(mensagem);
+
+    document.getElementById("media").innerHTML = "A Média da tua Nota é: " + media + "%";
   }
