ffe27359d6be13324407d1c71e674064448760be
IuriFrancezz <69679783+IuriFrancezz@users.noreply.github.com>
2020-11-29 00:41:05 +0000

    Fixed Calculating Errors

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
-function confirmarNota (nota1, nota2, nota3, nota4, media) {
+function confirmarNota() {
     var nota1 = document.getElementById("nota1").value;
     var nota2 = document.getElementById("nota2").value;
     var nota3 = document.getElementById("nota3").value;
     var nota4 = document.getElementById("nota4").value;
 
 
-    media = (nota1 + nota2 + nota3 + nota4) / 4;
+    notasValue = parseInt(nota1) + parseInt(nota2) + parseInt(nota3) + parseInt(nota4);
+    let media = notasValue / 4
+    
 
-    if( media < 10) {
+    if( media < 10 ) {
       mensagem = "A Média da tua Nota é " + media + " \n\n-> Reprovas";
       }
       else if (media >= 10 && media < 14) {
       mensagem = "A Média da tua Nota é " + media + " \n\n-> Vai a exame";
       }
       else {
-      mensagem = "A Média da tua Nota é " + media + " \n\n-> Dispensa de exame";
+      mensagem = "A Média da tua Nota é " + media + " \n\n-> Dispensado do exame";
       }
       
       alert(mensagem);
 
     document.getElementById("media").innerHTML = "A Média da tua Nota é: " + media + "%";
-  }
+}
